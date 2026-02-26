3cce8e4d022438673acab70609896f7d44a070fc
lauri000 <kajakko.lauri@gmail.com>
2026-02-26 22:23:35 +0200

    `htree pr` command to manage pull requests

--- rust/crates/hashtree-cli/src/app/mod.rs
+++ rust/crates/hashtree-cli/src/app/mod.rs
@@ -6,6 +6,7 @@ pub(crate) mod lists;
 #[cfg(feature = "fuse")]
 pub(crate) mod mount;
 pub(crate) mod peers;
+pub(crate) mod pr;
 pub(crate) mod resolve;
 pub(crate) mod socialgraph;
 pub(crate) mod util;
